7eeeed62622c8d70e5792f267ad93d5c5e3ce4e4
zandradatrilhu8 <zandradatrilhu8@gmx.com>
2025-03-03 13:38:59 +0000

    Add 1.c

---
+++ src/1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+  int x = 5;
+  if (x > 0) {
+    printf("The value of x is positive.\n");
+  } else {
+    printf("The value of x is not positive.\n");
+  }
+  return 0;
+}
